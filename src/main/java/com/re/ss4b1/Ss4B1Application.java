b47ea82f9d7039927d51c62977176f4a5bdd381e
LLL <Zeikezan12345@gmail.com>
2026-05-18 20:26:21 +0700

    B1

---
+++ src/main/java/com/re/ss4b1/Ss4B1Application.java
@@ -0,0 +1,13 @@
+package com.re.ss4b1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss4B1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ss4B1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/re/ss4b1/Ss4B1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.re.ss4b1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss4B1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
